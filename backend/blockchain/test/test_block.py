7539f32c6cb1d1080368916725d5f9b25c1619d0
inwisloklem <89099109104@mail.ru>
2020-01-24 17:16:27 +0300

    chore(block): change tests order

--- backend/blockchain/test/test_block.py
+++ backend/blockchain/test/test_block.py
@@ -5,21 +5,6 @@ from backend.blockchain.block import GENESIS_DATA, Block
 DATA = "second block data"
 
 
-def test_mine_block_slowly_adjust_difficulty():
-    last_block = Block.make_genesis_block()
-    time.sleep(MINE_RATE / SECONDS_MULTIPLIER)
-    block = Block.mine_block(last_block, DATA)
-
-    assert block.difficulty < last_block.difficulty
-
-
-def test_mine_block_quickly_adjust_difficulty():
-    last_block = Block.make_genesis_block()
-    block = Block.mine_block(last_block, DATA)
-
-    assert block.difficulty > last_block.difficulty
-
-
 def test_make_genesis_block():
     genesis_block = Block.make_genesis_block()
 
@@ -37,3 +22,18 @@ def test_mine_block():
     assert block.data == DATA
     assert block.last_block_hash == last_block.block_hash
     assert block.block_hash[0:block.difficulty] == "0" * block.difficulty
+
+
+def test_mine_block_quickly_adjust_difficulty():
+    last_block = Block.make_genesis_block()
+    block = Block.mine_block(last_block, DATA)
+
+    assert block.difficulty > last_block.difficulty
+
+
+def test_mine_block_slowly_adjust_difficulty():
+    last_block = Block.make_genesis_block()
+    time.sleep(MINE_RATE / SECONDS_MULTIPLIER)
+    block = Block.mine_block(last_block, DATA)
+
+    assert block.difficulty < last_block.difficulty
