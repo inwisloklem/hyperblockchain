f07147431556d0807e1c7c8d913c7c4641b5c1cb
inwisloklem <89099109104@mail.ru>
2020-01-30 17:59:37 +0300

    feat(main): add blockchain validation

--- backend/blockchain/blockchain.py
+++ backend/blockchain/blockchain.py
@@ -1,5 +1,6 @@
 from backend.blockchain.block import Block
-from pydash import last
+from backend.blockchain.errors import BlockchainValidationError, BlockValidationError
+from pydash import head, last
 
 
 class Blockchain:
@@ -16,3 +17,20 @@ class Blockchain:
     def add_block(self, data):
         block = Block.mine_block(last(self.chain), data)
         self.chain.append(block)
+
+    @staticmethod
+    def validate_blockchain(chain):
+        """
+        Validate the incoming chain by the following rules:
+            - must start with the genesis block
+            - blocks must be formatted correctly
+        """
+        if head(chain) != Block.make_genesis_block():
+            raise BlockchainValidationError("Genesis block in the head of chain is invalid")
+
+        try:
+            for i, block in enumerate(chain[1:]):
+                last_block = chain[i - 1]
+                Block.validate_block(last_block, block)
+        except BlockValidationError:
+            raise BlockchainValidationError("Blocks are not formatted correctly")
