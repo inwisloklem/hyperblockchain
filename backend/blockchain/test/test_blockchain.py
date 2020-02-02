3b8f6642c0646e97777b92607fe2c8c67744fe45
inwisloklem <89099109104@mail.ru>
2020-02-02 14:58:31 +0300

    feat(main): add tests for blockchain validation logic

--- backend/blockchain/test/test_blockchain.py
+++ backend/blockchain/test/test_blockchain.py
@@ -1,14 +1,26 @@
+import pytest
 from backend.blockchain.blockchain import Blockchain
 from backend.blockchain.block import GENESIS_DATA
+from backend.blockchain.errors import BlockchainValidationError
 from pydash import head, last
 
+DATA = "second block data"
+MALICIOUS_DATA = "malicious data"
 
-def test_add_block():
+
+@pytest.fixture
+def two_blocks_blockchain():
+    """
+    Generate blockchain of two blocks for tests
+    """
     blockchain = Blockchain()
-    data = "second block data"
-    blockchain.add_block(data)
+    blockchain.add_block(DATA)
 
-    assert last(blockchain.chain).data == data
+    return blockchain
+
+
+def test_add_block(two_blocks_blockchain):
+    assert last(two_blocks_blockchain.chain).data == DATA
 
 
 def test_blockchain_instance():
@@ -17,3 +29,21 @@ def test_blockchain_instance():
 
     assert isinstance(blockchain, Blockchain)
     assert genesis_block.data == GENESIS_DATA["data"]
+
+
+def test_validate_blockchain(two_blocks_blockchain):
+    assert Blockchain.validate_blockchain(two_blocks_blockchain.chain) is None
+
+
+def test_validate_blockchain_genesis_block_is_invalid(two_blocks_blockchain):
+    two_blocks_blockchain.chain[0].data = MALICIOUS_DATA
+
+    with pytest.raises(BlockchainValidationError):
+        Blockchain.validate_blockchain(two_blocks_blockchain.chain)
+
+
+def test_validate_blockchain_block_is_invalid(two_blocks_blockchain):
+    two_blocks_blockchain.chain[1].data = MALICIOUS_DATA
+
+    with pytest.raises(BlockchainValidationError):
+        Blockchain.validate_blockchain(two_blocks_blockchain.chain)
