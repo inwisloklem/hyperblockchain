be238c54abbabf75bd766ed811d9ec53c3491236
inwisloklem <89099109104@mail.ru>
2020-02-03 09:18:25 +0300

    feat(main): add blockchain replacement error

--- backend/blockchain/errors.py
+++ backend/blockchain/errors.py
@@ -1,3 +1,11 @@
+class BlockchainReplacementError(Exception):
+    """
+    Throw if the chain replacement has failed
+    """
+    def __init__(self, *args, **kwargs):
+        Exception(*args, **kwargs)
+
+
 class BlockchainValidationError(Exception):
     """
     Throw if the blockchain validation has failed
