187c585f6603c00e37008e8b69eb27eec5f9b183
inwisloklem <89099109104@mail.ru>
2020-02-11 22:13:16 +0300

    feat(app): add mine block api endpoint

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask, jsonify
 from backend.blockchain.blockchain import Blockchain
+from pydash import last
+
+TRANSACTION_DATA = 'stubbed transaction data'
 
 blockchain = Blockchain()
-
-for i in range(4):
-    blockchain.add_block(f"block {i}")
 
 app = Flask(__name__)
 app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
@@ -13,6 +13,12 @@ app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
 @app.route("/blockchain")
 def get_blockchain():
     return jsonify(blockchain.to_json())
+
+
+@app.route("/blockchain/mine")
+def get_blockchain_mine():
+    blockchain.add_block(TRANSACTION_DATA)
+    return jsonify(last(blockchain.chain).to_json())
 
 
 app.run(port=4444)
