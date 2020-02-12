534d028b57dff0bad02ca652c7b3af376d66bfa1
inwisloklem <89099109104@mail.ru>
2020-02-12 21:33:38 +0300

    feat(app): add peer mode

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,15 @@
+import os
+import random
 from flask import Flask, jsonify
 from backend.blockchain.blockchain import Blockchain
+from backend.pubsub import PubSub
 from pydash import last
 
+DEFAULT_PORT = 4444
 TRANSACTION_DATA = 'stubbed transaction data'
 
 blockchain = Blockchain()
+pubsub = PubSub("DEFAULT_CHANNEL")
 
 app = Flask(__name__)
 app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
@@ -21,4 +26,13 @@ def get_blockchain_mine():
     return jsonify(last(blockchain.chain).to_json())
 
 
-app.run(port=4444)
+def generate_port(is_peer, min_port=4444, max_port=8888):
+    """
+    Generate port number from `min_port` to `max_port` if in peer mode.
+    Else use default port number
+    """
+    return random.randint(min_port, max_port) if is_peer else DEFAULT_PORT
+
+
+is_peer = os.environ.get("PEER") == "True"
+app.run(port=generate_port(is_peer))
