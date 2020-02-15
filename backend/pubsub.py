e157fc9ea65eec85d046cafc49386c372ea94888
inwisloklem <89099109104@mail.ru>
2020-02-15 22:52:04 +0300

    feat(pubsub): add subscription to list of channels

--- backend/pubsub.py
+++ backend/pubsub.py
@@ -7,6 +7,9 @@ pnconfig = PNConfiguration()
 pnconfig.publish_key = "pub-c-f873c39d-8355-4cbb-894c-591e57988b63"
 pnconfig.subscribe_key = "sub-c-59f3e16c-4d03-11ea-814d-0ecb550e9de2"
 
+BLOCK_CHANNEL = "BLOCK_CHANNEL"
+DEFAULT_CHANNEL = "DEFAULT_CHANNEL"
+
 
 class Listener(SubscribeCallback):
     def message(self, pubnub, message_object):
@@ -18,21 +21,20 @@ class PubSub():
     Handle the publish/subscribe layer of the application.
     Provide communication between the nodes of the blockchain network
     """
-    def __init__(self, channel, pnconfig=pnconfig):
-        self.channel = channel
+    def __init__(self, channels, pnconfig=pnconfig):
         self.pubnub = PubNub(pnconfig)
-        self.pubnub.subscribe().channels([channel]).execute()
+        self.pubnub.subscribe().channels(channels).execute()
         self.pubnub.add_listener(Listener())
 
-    def publish(self, message):
+    def publish(self, channel, message):
         """
         Publish the message to the channel
         """
-        self.pubnub.publish().channel(self.channel).message(message).sync()
+        self.pubnub.publish().channel(channel).message(message).sync()
 
 
 if __name__ == "__main__":
-    pubsub = PubSub("TEST_CHANNEL")
+    pubsub = PubSub([DEFAULT_CHANNEL])
 
     time.sleep(1)
-    pubsub.publish({"test": "message"})
+    pubsub.publish(DEFAULT_CHANNEL, {"test": "message"})
