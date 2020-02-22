de43c90d5429476e8ec2f040c9b5c4ddce542824
inwisloklem <89099109104@mail.ru>
2020-02-22 10:58:10 +0300

    feat(wallet): add sign method

--- backend/wallet/wallet.py
+++ backend/wallet/wallet.py
@@ -1,6 +1,8 @@
+import json
 from backend.config import STARTING_BALANCE
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives import hashes
 from uuid import uuid4
 
 
@@ -15,6 +17,13 @@ class Wallet:
         self.private_key = ec.generate_private_key(ec.SECP256K1(), default_backend())
         self.public_key = self.private_key.public_key()
 
+    def sign(self, data):
+        """
+        Generate a signature based on the data using private key
+        """
+        encoded_data = json.dumps(data).encode("utf-8")
+        return self.private_key.sign(encoded_data, ec.ECDSA(hashes.SHA256()))
+
     @staticmethod
     def generate_uuid():
         """
@@ -27,3 +36,8 @@ class Wallet:
 if __name__ == "__main__":
     wallet = Wallet()
     print(wallet.__dict__)
+
+    data = {"test": "message"}
+    signature = wallet.sign(data)
+
+    print(f"Signature: {signature}")
