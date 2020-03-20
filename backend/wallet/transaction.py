c66b4f7e1d6a5d7de94eb94e10f62fd4a798083f
inwisloklem <89099109104@mail.ru>
2020-03-20 19:25:02 +0300

    feat(wallet): add update method

--- backend/wallet/transaction.py
+++ backend/wallet/transaction.py
@@ -21,11 +21,11 @@ class Transaction():
         and `address`
         """
         input = {
-            'address': sender_wallet.address,
-            'amount': sender_wallet.balance,
-            'public_key': sender_wallet.public_key,
-            'signature': sender_wallet.sign(output),
-            'timestamp': time.time_ns(),
+            "address": sender_wallet.address,
+            "amount": sender_wallet.balance,
+            "public_key": sender_wallet.public_key,
+            "signature": sender_wallet.sign(output),
+            "timestamp": time.time_ns(),
         }
 
         return input
@@ -40,7 +40,31 @@ class Transaction():
 
         return output
 
+    def update(self, sender_wallet, recipient_address, amount):
+        """
+        Update the transaction with an existing or new recipient
+        """
+        if amount > self.output[sender_wallet.address]:
+            raise AmountExceedsBalanceError
 
-if __name__ == '__main__':
-    transaction = Transaction(Wallet(), generate_uuid(), 15)
+        if recipient_address in self.output:
+            self.output[recipient_address] = self.output[recipient_address] + amount
+        else:
+            self.output[recipient_address] = amount
+
+        self.output[sender_wallet.address] = self.output[sender_wallet.address] - amount
+        self.input = self.make_input(sender_wallet, self.output)
+
+
+if __name__ == "__main__":
+    sender_wallet = Wallet()
+    first_recipient_address = generate_uuid()
+    transaction = Transaction(sender_wallet, first_recipient_address, 15)
+    print(f"transaction: {transaction.__dict__}\n")
+
+    transaction.update(sender_wallet, first_recipient_address, 10)
+    print(f"transaction: {transaction.__dict__}\n")
+
+    second_recipient_address = generate_uuid()
+    transaction.update(sender_wallet, second_recipient_address, 10)
     print(f"transaction: {transaction.__dict__}")
