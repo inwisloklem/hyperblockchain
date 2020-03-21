033741f2a064790e699d4e732dc26f6f969e54fc
inwisloklem <89099109104@mail.ru>
2020-03-21 22:37:26 +0300

    feat(wallet): add tests for update method

--- backend/wallet/test/test_transaction.py
+++ backend/wallet/test/test_transaction.py
@@ -19,12 +19,12 @@ def recipient_address_sender_wallet_pair():
 def test_transaction(recipient_address_sender_wallet_pair):
     amount = 75
     recipient_address, sender_wallet = recipient_address_sender_wallet_pair
+    balance_left = sender_wallet.balance - amount
     transaction = Transaction(sender_wallet, recipient_address, amount)
     input, output = transaction.input, transaction.output
-    left = sender_wallet.balance - amount
 
     assert transaction.output[recipient_address] == amount
-    assert transaction.output[sender_wallet.address] == left
+    assert transaction.output[sender_wallet.address] == balance_left
 
     assert transaction.input["address"] == sender_wallet.address
     assert transaction.input["amount"] == sender_wallet.balance
@@ -40,3 +40,38 @@ def test_transaction_amount_exceeds_balance(recipient_address_sender_wallet_pair
 
     with pytest.raises(AmountExceedsBalanceError):
         Transaction(sender_wallet, recipient_address, amount)
+
+
+def test_transaction_update(recipient_address_sender_wallet_pair):
+    amount_before_update, amount_after_update = 10, 15
+    recipient_address, sender_wallet = recipient_address_sender_wallet_pair
+    transaction = Transaction(sender_wallet, recipient_address, amount_before_update)
+
+    balance_left = sender_wallet.balance - amount_before_update - amount_after_update
+    transaction.update(sender_wallet, recipient_address, amount_after_update)
+
+    assert transaction.output[sender_wallet.address] == balance_left
+    assert transaction.output[recipient_address] == amount_before_update + amount_after_update
+
+
+def test_transaction_update_another_recipient(recipient_address_sender_wallet_pair):
+    amount_before_update, amount_after_update = 10, 15
+    another_recipient_address = generate_uuid()
+    recipient_address, sender_wallet = recipient_address_sender_wallet_pair
+    transaction = Transaction(sender_wallet, recipient_address, amount_before_update)
+
+    balance_left = sender_wallet.balance - amount_before_update - amount_after_update
+    transaction.update(sender_wallet, another_recipient_address, amount_after_update)
+
+    assert transaction.output[sender_wallet.address] == balance_left
+    assert transaction.output[recipient_address] == amount_before_update
+    assert transaction.output[another_recipient_address] == amount_after_update
+
+
+def test_transaction_update_amount_exceeds_balance(recipient_address_sender_wallet_pair):
+    amount_before_update, amount_after_update = 10, 150
+    recipient_address, sender_wallet = recipient_address_sender_wallet_pair
+    transaction = Transaction(sender_wallet, recipient_address, amount_before_update)
+
+    with pytest.raises(AmountExceedsBalanceError):
+        transaction.update(sender_wallet, recipient_address, amount_after_update)
